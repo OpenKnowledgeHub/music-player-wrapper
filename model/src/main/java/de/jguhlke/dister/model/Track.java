ca630abc3388f10dde0e05f2f88e8a0932eceb8a
Jelmen Guhlke <mail@jguhlke.de>
2024-09-08 08:06:05 +0200

    feat(model): add Track implementation

--- model/src/main/java/de/jguhlke/dister/model/Track.java
+++ model/src/main/java/de/jguhlke/dister/model/Track.java
@@ -1,3 +1,16 @@
 package de.jguhlke.dister.model;
 
-public record Track(TrackId id, String name) {}
+import de.jguhlke.dister.model.exception.DisterException;
+
+import java.util.Objects;
+
+public record Track(TrackId id, String name) {
+  public Track {
+    Objects.requireNonNull(id, "'id' must be set");
+    Objects.requireNonNull(name, "'name' must be set");
+
+    if (name.isBlank()) {
+      throw new DisterException("'name' must not be blank");
+    }
+  }
+}
---
+++ model/src/test/java/de/jguhlke/dister/model/TrackTest.java
@@ -0,0 +1,54 @@
+package de.jguhlke.dister.model;
+
+import de.jguhlke.dister.model.exception.DisterException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("Test Track entity")
+class TrackTest {
+
+  final String testTrackName = "Hello";
+  final TrackId testTrackId = new TrackId("track:1234");
+
+  @Nested
+  @DisplayName("Test Track creation")
+  public class TrackCreationTest {
+    @Test
+    @DisplayName("Should create a track")
+    public void testCreateTrack() {
+      final var createdTrack = new Track(testTrackId, testTrackName);
+
+      assertThat(createdTrack).isNotNull();
+      assertThat(createdTrack.id()).isEqualTo(testTrackId);
+      assertThat(createdTrack.name()).isEqualTo(testTrackName);
+    }
+
+    @Test
+    @DisplayName("Should not create a track without id")
+    public void testCreateTrackWithoutId() {
+      assertThatThrownBy(() -> new Track(null, testTrackName))
+          .isInstanceOf(NullPointerException.class)
+          .hasMessage("'id' must be set");
+    }
+
+    @Test
+    @DisplayName("Should not create a track without name")
+    public void testCreateTrackWithoutName() {
+      assertThatThrownBy(() -> new Track(testTrackId, null))
+          .isInstanceOf(NullPointerException.class)
+          .hasMessage("'name' must be set");
+    }
+
+    @Test
+    @DisplayName("Should not create a track without name content")
+    public void testCreateTrackWithoutNameContent() {
+      assertThatThrownBy(() -> new Track(testTrackId, ""))
+              .isInstanceOf(DisterException.class)
+              .hasMessage("'name' must not be blank");
+    }
+  }
+}
